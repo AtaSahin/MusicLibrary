68352e1ca42e0e2a34c3d09d54639f7c9fd04085
Ata Sahin <atasahin05@gmail.com>
2023-12-11 17:25:16 +0300

    Login, Register pages have been created..DB connection provided

---
+++ Models/Song.cs
@@ -0,0 +1,15 @@
+﻿namespace MusicLibraryApp.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Artist { get; set; }
+        public string Genre { get; set; }
+        public string CoverImage { get; set; }
+        public string AudioFile { get; set; }
+        public string AddedByUserId { get; set; }
+      
+    }
+
+}
---
+++ Services/MusicApiService.cs
@@ -0,0 +1,21 @@
+﻿using MusicLibraryApp.Models;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+public class MusicApiService
+{
+    private readonly HttpClient _httpClient;
+
+    public MusicApiService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<List<Song>> GetSongs()
+    {
+        var response = await _httpClient.GetFromJsonAsync<List<Song>>("/api/songs");
+        return response;
+    }
+}
