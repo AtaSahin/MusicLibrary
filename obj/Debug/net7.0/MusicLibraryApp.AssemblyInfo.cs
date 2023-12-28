3dcbfb6484a129d8e5b4f87574101c8b133fc817
Ata Sahin <atasahin05@gmail.com>
2023-12-28 14:18:37 +0300

    rabbitMQ configurations added

--- obj/Debug/net7.0/MusicLibraryApp.AssemblyInfo.cs
+++ obj/Debug/net7.0/MusicLibraryApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicLibraryApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+349f558004db581b9785dee35f389014a60015de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicLibraryApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicLibraryApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
